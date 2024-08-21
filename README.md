63a6d3d6efe5f0a10a823ac9fb5d5e9f15e7e92d
shantih19 <shantih19@shantih19.xyz>
2024-08-21 20:48:49 +0200

    automatically find last page allow for download of other resources, not only wallpapers

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Sonic Channel Scraper
 
-Mass download Sonic Channel Wallpapers
+Mass download Sonic Channel files.
 
 ## Installation
 
@@ -9,17 +9,27 @@ Install with `pip install sonic-channel-scraper` or with `poetry install`.
 ## Usage
 
 ```text
-usage: sonic_channel_scraper [-h] [-p PAGES] [--tar] [--pc | --mobile] [-c CHARACTER] dir
+usage: sonic_channel_scraper [-h] (--all | --wallpaper | --icon | --calendar | --coloring | --papercraft | --letterset) [--tar] [-c CHARACTER] [--pc | --mobile] dir
 
 positional arguments:
   dir                   Directory to save into.
 
 options:
   -h, --help            show this help message and exit
-  -p PAGES, --pages PAGES
+
+Types:
+  --all                 Scrape all resource types.
+  --wallpaper           Scrape wallpapers.
+  --icon                Scrape icons.
+  --calendar            Scrape calendar pages.
+  --coloring            Scrape coloring pages.
+  --papercraft          Scrape papercraft sets.
+  --letterset           Scrape lettering sets.
+
+Options:
   --tar                 Save all images into .tar.gz archive.
-  --pc                  Download only PC wallpapers.
-  --mobile              Download only mobile wallpapers.
   -c CHARACTER, --character CHARACTER
                         Filter for character. Can be repeated for multiple.
+  --pc                  Download only PC wallpapers.
+  --mobile              Download only mobile wallpapers.
 ```
--- sonic_channel_scraper/__init__.py
+++ sonic_channel_scraper/__init__.py
@@ -2,7 +2,7 @@
 
 import argparse
 import tarfile
-from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+from concurrent.futures import Future, ProcessPoolExecutor, ThreadPoolExecutor
 from itertools import chain
 from pathlib import Path
 from urllib.parse import urlparse
@@ -13,7 +13,7 @@ import requests
 MAX_THREADS = 16
 
 BASE_URL = "https://sonic.sega.jp"
-LIST_URL = BASE_URL + "/SonicChannel/enjoy/wallpaper/"
+LIST_URL = BASE_URL + "/SonicChannel/enjoy/"
 
 session = requests.session()
 
@@ -32,9 +32,62 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("dir", type=arg_dir, help="Directory to save into.")
 
-parser.add_argument("-p", "--pages", default=23, type=int)
+types_group = parser.add_argument_group(title="Types")
+types = types_group.add_mutually_exclusive_group(required=True)
 
-archives = parser.add_mutually_exclusive_group()
+types.add_argument(
+    "--all",
+    action="store_const",
+    const="",
+    dest="type",
+    help="Scrape all resource types.",
+)
+types.add_argument(
+    "--wallpaper",
+    action="store_const",
+    const="wallpaper/",
+    dest="type",
+    help="Scrape wallpapers.",
+)
+types.add_argument(
+    "--icon",
+    action="store_const",
+    const="icon/",
+    dest="type",
+    help="Scrape icons.",
+)
+types.add_argument(
+    "--calendar",
+    action="store_const",
+    const="calendar/",
+    dest="type",
+    help="Scrape calendar pages.",
+)
+types.add_argument(
+    "--coloring",
+    action="store_const",
+    const="coloring/",
+    dest="type",
+    help="Scrape coloring pages.",
+)
+types.add_argument(
+    "--papercraft",
+    action="store_const",
+    const="papercraft/",
+    dest="type",
+    help="Scrape papercraft sets.",
+)
+types.add_argument(
+    "--letterset",
+    action="store_const",
+    const="letterset/",
+    dest="type",
+    help="Scrape lettering sets.",
+)
+
+options = parser.add_argument_group(title="Options")
+
+archives = options.add_mutually_exclusive_group()
 
 archives.add_argument(
     "--tar",
@@ -42,25 +95,28 @@ archives.add_argument(
     help="Save all images into .tar.gz archive.",
 )
 
-types = parser.add_mutually_exclusive_group()
-
-types.add_argument(
-    "--pc",
-    action="store_true",
-    help="Download only PC wallpapers.",
-)
-types.add_argument(
-    "--mobile",
-    action="store_true",
-    help="Download only mobile wallpapers.",
-)
-
-parser.add_argument(
+options.add_argument(
     "-c",
     "--character",
     action="append",
     help="Filter for character. Can be repeated for multiple.",
 )
+
+wallpaper_types = options.add_mutually_exclusive_group()
+
+wallpaper_types.add_argument(
+    "--pc",
+    action="store_true",
+    help="Download only PC wallpapers.",
+)
+wallpaper_types.add_argument(
+    "--mobile",
+    action="store_true",
+    help="Download only mobile wallpapers.",
+)
+
+
+args = parser.parse_args()
 
 
 def get_page(page: int = 1) -> bytes:
@@ -73,7 +129,7 @@ def get_page(page: int = 1) -> bytes:
         bytes: Content of page.
     """
     index = f"index_{page}.html" if page != 1 else "index.html"
-    res: requests.Response = session.get(LIST_URL + index)
+    res: requests.Response = session.get(LIST_URL + args.type + index)
     res.raise_for_status()
     return res.content
 
@@ -112,9 +168,25 @@ def download_image(path: Path, url: str) -> Path:
 
 def main():
     """Main."""
-    args = parser.parse_args()
+    pages = []
+
+    def _save_future(future: Future):
+        if future.done():
+            pages.append(future.result())
+
     with ThreadPoolExecutor(max_workers=MAX_THREADS) as exc:
-        pages = exc.map(get_page, range(1, args.pages + 1))
+        page = 1
+        while True:
+            try:
+                future = exc.submit(get_page, page)
+                exception = future.exception()
+                if exception is not None:
+                    raise exception
+            except requests.HTTPError as e:
+                if e.response.status_code == 404:
+                    break
+            future.add_done_callback(_save_future)
+            page += 1
     with ProcessPoolExecutor() as exc:
         res = exc.map(get_image_urls, pages)
     urls = chain.from_iterable(res)
